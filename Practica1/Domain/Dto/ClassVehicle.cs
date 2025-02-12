6c2b8c6158a4c053cdafb23ee1262cf394988eaa
juan1241 <2021-0330@uteco.edu.do>
2025-02-11 19:11:53 -0800

    Agregar archivos de proyecto.

---
+++ Practica1/Domain/Dto/ClassVehicle.cs
@@ -0,0 +1,10 @@
+﻿namespace Practica1.Domain.Dto
+{
+	public class ClassVehicle
+	{
+		public int Id { get; set; }
+		public string Marca { get; set; }
+		public string Modelo { get; set; }
+		public int Año { get; set; }
+	}
+}
